f6795699b380c8188b56a7a5d7a3c7c90d1680b6
aungkyawkyawsoe <akks.ygn@gmail.com>
2021-09-22 15:10:09 +0630

    setup workspace

---
+++ libs/header/src/lib/header.tsx
@@ -0,0 +1,14 @@
+import './header.module.scss';
+
+/* eslint-disable-next-line */
+export interface HeaderProps {}
+
+export function Header(props: HeaderProps) {
+  return (
+    <div>
+      <h1>Hello Par Lone</h1>
+    </div>
+  );
+}
+
+export default Header;
